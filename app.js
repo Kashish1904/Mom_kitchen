056e070dbe241c8b2e97be48e99f9c7806bb5fbc
Kashish1904 <abhishekjain7204@gmail.com>
2020-05-12 00:07:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -51,6 +51,6 @@ app.post("/index.html", function(req, res){
   res.redirect("/");
 });
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Started on 3000");
 });

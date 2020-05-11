cb0d633cdf92303d4c23f8a720b143c21ed48352
Kashish1904 <abhishekjain7204@gmail.com>
2020-05-11 23:34:44 +0530

    DB

---
+++ app.js
@@ -0,0 +1,56 @@
+
+const express=require("express");
+const bodyParser=require("express");
+const mongoose=require("mongoose");
+const app=express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb+srv://admin:admin@cluster0-etblv.mongodb.net/test?retryWrites=true&w=majority/momkitchenDB", {useNewUrlParser: true});
+
+const commentsSchema = {
+  name: String,
+  email: String,
+  phone: Number,
+  message: String
+}
+
+const Message = mongoose.model("Message", commentsSchema);
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname+"/index.html")
+});
+
+app.get("/index.html", function(req, res){
+  res.sendFile(__dirname+"/index.html")
+});
+
+app.get("/Veggies.html", function(req, res){
+  res.sendFile(__dirname+"/Veggies.html")
+});
+
+app.get("/Salads.html", function(req, res){
+  res.sendFile(__dirname+"/Salads.html")
+});
+
+app.get("/Snacks.html", function(req, res){
+  res.sendFile(__dirname+"/Snacks.html")
+});
+
+app.get("/Desserts.html", function(req, res){
+  res.sendFile(__dirname+"/Desserts.html")
+});
+
+app.get("/sugg.html", function(req, res){
+  res.sendFile(__dirname+"/Sugg.html")
+});
+
+app.post("/index.html", function(req, res){
+  Message.insertMany(req.body);
+  res.redirect("/");
+});
+
+app.listen(3000, function(){
+  console.log("Started on 3000");
+});
